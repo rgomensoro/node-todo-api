59d57750c7cf648b8ccba8ce15f6cb3791db1655
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2018-08-05 19:57:32 -0300

    update file adjusted

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -12,23 +12,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) =>{
 
     const db = client.db('TodoApp');
  
-
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5b6771f50b09423f0c87b08c')
-    // }, { $set: {
-    //     completed: true
-    //         }
-    //     }, {
-    //        returnOriginal: false 
-    //     }).then ((res) => {
-    
-    //          console.log(JSON.stringify(res, undefined,2));
-    
-    //  }, (err) => {
-    //          console.log('Unable to update todos',err);
-
-    // });
-
     
     db.collection('Users').findOneAndUpdate({
         _id: new ObjectID('5b6622b146f3f8159040a334')
