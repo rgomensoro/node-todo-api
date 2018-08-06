853201e2258b53f08c0d0ea37825f54d50e2f5d5
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2018-08-05 22:39:24 -0300

    Todo App Server structure update

--- server/server.js
+++ server/server.js
@@ -1,41 +1,21 @@
-var mongoose = require ('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require ('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: { 
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('todos', (req,res)=>{
+    console.log (req.body);
 });
 
-//User model
-var User = mongoose.model('User', {
-    email: { 
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
+
+app.listen(3000, () => {
+    console.log('Started. Listen on port 3.000');
 });
 
-var newUser = new User({
-    email: '  nandagomen@gmail.com   ',
-});
-
-newUser.save().then((doc) => {
-    console.log (`save user: ${doc}`);
-}, (e) =>{
-    console.log(`Unable do save user: "${e.message}"`);
-});
