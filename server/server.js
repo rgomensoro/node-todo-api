b8484928b30c635df16204f22396db19476e141f
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2018-08-07 17:38:37 -0300

    GET /todos route implemented

--- server/server.js
+++ server/server.js
@@ -24,6 +24,17 @@ app.post('/todos', (req,res)=>{
 
 });
 
+app.get('/todos', (req,res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send({todos});
+
+    }, e =>{
+        res.status(400).send(`Error. ${e.message}`);
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started. Listen on port 3.000');
